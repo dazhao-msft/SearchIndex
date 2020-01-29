19bea5c84b789cf565d73098c9ea11650802e737
David Zhao (DEVDIV) <dazhao@microsoft.com>
2020-01-29 14:27:05 -0800

    initial

---
+++ SearchIndex/SearchIndex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SearchIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
