0c272e632a043bb16ce684dc432bd3ffdcac9492
David Zhao (DEVDIV) <dazhao@microsoft.com>
2020-02-03 11:40:09 -0800

    save

--- SearchIndex/IndexBuilder/Document.cs
+++ SearchIndex/IndexBuilder/Document.cs
@@ -8,57 +8,67 @@ namespace IndexBuilder
     {
         [Key]
         [IsFilterable]
-        public string Id { get; set; }
+        [JsonProperty("id")]
+        public string EntityId { get; set; }
 
-        [IsFilterable]
-        public string OrganizationId { get; set; }
-
-        public string RecordAsJson { get; set; }
+        [IsSearchable, IsFilterable, IsFacetable]
+        [JsonProperty("primary_field")]
+        public string EntityPrimaryField { get; set; }
 
         [IsFilterable, IsFacetable]
-        [JsonProperty("account")]
-        public string EntityName { get; set; }
+        [JsonProperty("type")]
+        public string EntityType { get; set; }
+
+        [JsonProperty("entity")]
+        public string EntityAsJson { get; set; }
 
         //
-        // The following fields are indexed for NLU. They are hardcoded in this prototype.
+        // The following fields are indexed for NLU. They are hardcoded in the prototype.
         // In production, a better design would be to define a collection of generic index fields,
         // and use a metadata mapping table to dynamically map the selected fields to the index.
+        //
+        // CDS entity names plus attribute names are defined as the property name in the following properties:
+        // `[JsonProperty("{CDSEntityName}_{CDSAttributeName}")]`
         //
 
         #region Account 
 
         [IsSearchable, IsFilterable, IsFacetable]
-        [JsonProperty("name")]
-        public string AccountName { get; set; }
-
-        [IsSearchable, IsFilterable, IsFacetable]
-        [JsonProperty("address1_city")]
+        [JsonProperty("account_address1_city")]
         public string AccountAddress1City { get; set; }
 
         [IsSearchable, IsFilterable, IsFacetable]
-        [JsonProperty("address1_state")]
+        [JsonProperty("account_address1_state")]
         public string AccountAddress1State { get; set; }
 
         [IsSearchable, IsFilterable, IsFacetable]
-        [JsonProperty("address1_country")]
+        [JsonProperty("account_address1_country")]
         public string AccountAddress1Country { get; set; }
 
         [IsSearchable, IsFilterable, IsFacetable]
-        [JsonProperty("telephone1")]
+        [JsonProperty("account_telephone1")]
         public string AccountTelephone1 { get; set; }
 
         [IsSearchable, IsFilterable, IsFacetable]
-        [JsonProperty("emailaddress1")]
-        public string AccountEMailAddress1 { get; set; }
+        [JsonProperty("account_emailaddress1")]
+        public string AccountEmailAddress1 { get; set; }
 
         [IsSearchable, IsFilterable, IsFacetable]
-        [JsonProperty("tickersymbol")]
+        [JsonProperty("account_tickersymbol")]
         public string AccountTickerSymbol { get; set; }
 
         [IsSearchable, IsFilterable, IsFacetable]
-        [JsonProperty("websiteurl")]
+        [JsonProperty("account_websiteurl")]
         public string AccountWebSite { get; set; }
 
-        #endregion
+        #endregion Account
+
+        #region Opportunity 
+
+        [IsSearchable, IsFilterable, IsFacetable]
+        [JsonProperty("opportunity_stepname")]
+        public string OpportunityStepName { get; set; }
+
+        #endregion Opportunity
     }
 }
