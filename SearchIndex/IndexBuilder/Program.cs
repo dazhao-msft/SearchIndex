8f930e63e150e1c548a7a9b8bbbb6496241aa112
David Zhao (DEVDIV) <dazhao@microsoft.com>
2020-02-03 14:00:23 -0800

    save

--- SearchIndex/IndexBuilder/Program.cs
+++ SearchIndex/IndexBuilder/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using Microsoft.Azure.Search;
+using Microsoft.Extensions.Configuration;
+using Index = Microsoft.Azure.Search.Models.Index;
 
 namespace IndexBuilder
 {
-    internal class Program
+    public static class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddUserSecrets(typeof(Program).Assembly);
+            var configuration = builder.Build();
+
+            var serviceClient = CreateSearchServiceClient(configuration);
+            var indexName = GetIndexName(configuration);
+
+            CreateOrUpdateIndexDefinition(serviceClient, indexName);
+        }
+
+        private static string GetIndexName(IConfiguration configuration)
+        {
+            return configuration["SearchIndexName"];
+        }
+
+        private static SearchServiceClient CreateSearchServiceClient(IConfiguration configuration)
+        {
+            string searchServiceName = configuration["SearchServiceName"];
+            string adminApiKey = configuration["SearchServiceAdminApiKey"];
+
+            SearchServiceClient serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(adminApiKey));
+            return serviceClient;
+        }
+
+        private static Index CreateOrUpdateIndexDefinition(ISearchServiceClient serviceClient, string indexName)
+        {
+            if (serviceClient.Indexes.Exists(indexName))
+            {
+                serviceClient.Indexes.Delete(indexName);
+            }
+
+            var definition = new Index()
+            {
+                Name = indexName,
+                Fields = FieldBuilder.BuildForType<Document>(),
+            };
+
+            return serviceClient.Indexes.Create(definition);
         }
     }
 }
