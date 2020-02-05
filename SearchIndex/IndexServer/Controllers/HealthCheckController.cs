20bf9c363957d2e572161305c6760c01005ac0c1
David Zhao (DEVDIV) <dazhao@microsoft.com>
2020-02-04 16:04:11 -0800

    add health check

---
+++ SearchIndex/IndexServer/Controllers/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IndexServer.Controllers
+{
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet("health/live")]
+        public ActionResult GetLiveness() => Ok();
+
+        [HttpGet("health/ready")]
+        public ActionResult GetReadiness() => Ok();
+    }
+}
