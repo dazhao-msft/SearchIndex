7571b4a19c72a286b12c17537c4eecbbaa8f6ac4
David Zhao (DEVDIV) <dazhao@microsoft.com>
2020-02-06 19:57:36 -0800

    save

--- SearchIndex/IndexServer/Services/MetadataSearchResultHandler.cs
+++ SearchIndex/IndexServer/Services/MetadataSearchResultHandler.cs
@@ -1,5 +1,8 @@
 ﻿using IndexModels;
+using IndexServer.Models;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace IndexServer.Services
@@ -24,6 +27,73 @@ namespace IndexServer.Services
                 if (entityName != Document.MetadataEntityName)
                 {
                     continue;
+                }
+
+                foreach (var highlight in searchResult.Highlights)
+                {
+                    if (highlight.Key == Document.MetadataEntityEntityFieldName)
+                    {
+                        string cdsEntityName = searchResult.Document[Document.MetadataEntityEntityFieldName].ToString();
+
+                        int startIndex = context.SearchText.IndexOf(cdsEntityName, StringComparison.OrdinalIgnoreCase);
+                        if (startIndex >= 0)
+                        {
+                            var matchedTerm = new MatchedTerm
+                            {
+                                Text = cdsEntityName,
+                                StartIndex = startIndex,
+                                Length = cdsEntityName.Length,
+                                TermBindings = new HashSet<TermBinding>(),
+                            };
+
+                            matchedTerm.TermBindings.Add(new TermBinding()
+                            {
+                                BindingType = BindingType.Table,
+                                SearchScope = new SearchScope()
+                                {
+                                    Table = cdsEntityName,
+                                    Column = null,
+                                },
+                                Value = cdsEntityName,
+                                IsExactlyMatch = true,
+                                IsSynonymMatch = false,
+                            });
+
+                            context.MatchedTerms.Add(matchedTerm);
+                        }
+                    }
+                    else if (highlight.Key == Document.MetadataEntityAttributeFieldName)
+                    {
+                        string cdsEntityName = searchResult.Document[Document.MetadataEntityEntityFieldName].ToString();
+                        string cdsAttributeName = searchResult.Document[Document.MetadataEntityAttributeFieldName].ToString();
+
+                        int startIndex = context.SearchText.IndexOf(cdsAttributeName, StringComparison.OrdinalIgnoreCase);
+                        if (startIndex >= 0)
+                        {
+                            var matchedTerm = new MatchedTerm
+                            {
+                                Text = cdsAttributeName,
+                                StartIndex = startIndex,
+                                Length = cdsAttributeName.Length,
+                                TermBindings = new HashSet<TermBinding>(),
+                            };
+
+                            matchedTerm.TermBindings.Add(new TermBinding()
+                            {
+                                BindingType = BindingType.Column,
+                                SearchScope = new SearchScope()
+                                {
+                                    Table = cdsEntityName,
+                                    Column = cdsAttributeName,
+                                },
+                                Value = cdsAttributeName,
+                                IsExactlyMatch = true,
+                                IsSynonymMatch = false,
+                            });
+
+                            context.MatchedTerms.Add(matchedTerm);
+                        }
+                    }
                 }
             }
         }
