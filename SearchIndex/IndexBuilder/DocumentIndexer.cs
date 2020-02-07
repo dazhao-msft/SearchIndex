980ee6c3e617ddbb1b3b1870083617360f6fbbb6
David Zhao (DEVDIV) <dazhao@microsoft.com>
2020-02-07 12:32:44 -0800

    adds logs

--- SearchIndex/IndexBuilder/DocumentIndexer.cs
+++ SearchIndex/IndexBuilder/DocumentIndexer.cs
@@ -13,12 +13,18 @@ namespace IndexBuilder
 {
     public static class DocumentIndexer
     {
-        public static async Task<Index> ForceCreateIndexAsync(ISearchServiceClient serviceClient, string indexName)
+        public static async Task ForceCreateIndexAsync(ISearchServiceClient serviceClient, string indexName)
         {
             if (await serviceClient.Indexes.ExistsAsync(indexName))
             {
+                Console.WriteLine($"Index '{indexName}' exists. Deleting the existing index...");
+
                 await serviceClient.Indexes.DeleteAsync(indexName);
+
+                Console.WriteLine($"Index '{indexName}' was deleted.");
             }
+
+            Console.WriteLine($"Creating index '{indexName}'...");
 
             var definition = new Index()
             {
@@ -26,7 +32,9 @@ namespace IndexBuilder
                 Fields = FieldBuilder.BuildForType<Document>(),
             };
 
-            return await serviceClient.Indexes.CreateAsync(definition);
+            await serviceClient.Indexes.CreateAsync(definition);
+
+            Console.WriteLine($"Index '{indexName}' was created.");
         }
 
         public static async Task UploadDocumentsAsync(ISearchServiceClient serviceClient, string indexName)
@@ -34,6 +42,8 @@ namespace IndexBuilder
             //
             // Prepare documents.
             //
+
+            Console.WriteLine("Preparing documents...");
 
             var documents = new List<Document>();
 
@@ -48,23 +58,25 @@ namespace IndexBuilder
             // Uploads documents.
             //
 
+            Console.WriteLine("Uploading documents...");
+
             var batch = IndexBatch.Upload(documents);
 
             try
             {
                 var indexClient = serviceClient.Indexes.GetClient(indexName);
 
-                indexClient.Documents.Index(batch);
+                await indexClient.Documents.IndexAsync(batch);
             }
             catch (IndexBatchException e)
             {
                 // Sometimes when your Search service is under load, indexing will fail for some of the documents in
                 // the batch. Depending on your application, you can take compensating actions like delaying and
                 // retrying. For this simple demo, we just log the failed document keys and continue.
-                Console.WriteLine(
-                    "Failed to index some of the documents: {0}",
-                    string.Join(", ", e.IndexingResults.Where(r => !r.Succeeded).Select(r => r.Key)));
+                Console.WriteLine("Failed to index some of the documents: {0}", string.Join(", ", e.IndexingResults.Where(r => !r.Succeeded).Select(r => r.Key)));
             }
+
+            Console.WriteLine("Uploading documents completed.");
         }
 
         private static async Task<IReadOnlyCollection<Document>> ReadEntitiesAsDocumentsAsync(string file, string entityName, string entityIdAttributeName)
