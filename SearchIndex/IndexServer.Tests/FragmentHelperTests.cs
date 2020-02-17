ca25156c8375630c6c0002ebe891cd727b4ba737
David Zhao (DEVDIV) <dazhao@microsoft.com>
2020-02-17 01:35:40 -0800

    start adding tests

--- SearchIndex/IndexServer.Tests/FragmentHelperTests.cs
+++ SearchIndex/IndexServer.Tests/FragmentHelperTests.cs
@@ -5,12 +5,19 @@ namespace IndexServer.Services.Tests
 {
     public class FragmentHelperTests
     {
-        [Fact]
-        public void TestGetTokensFromText()
+        [Theory]
+        [InlineData("", "<em>", "</em>", new string[] { })]
+        [InlineData("a <em>test</em> and another <em>test</em>", "<em>", "</em>", new[] { "test", "test" })]
+        public void TestGetTokensFromFragment(string fragment, string preTag, string postTag, string[] expected)
         {
-            var tokens = FragmentHelper.GetTokensFromFragment("a <em>test</em> and another <em>test</em>", "<em>", "</em>").ToList();
+            var tokens = FragmentHelper.GetTokensFromFragment(fragment, preTag, postTag).ToList();
 
-            Assert.True(tokens.Count > 0);
+            Assert.True(tokens.Count == expected.Length);
+
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                Assert.True(tokens[i].Token == expected[i]);
+            }
         }
     }
 }
