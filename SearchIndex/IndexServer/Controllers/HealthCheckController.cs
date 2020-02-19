7628aaffe73682323fa1e69450f3fd7d7ce216ad
David Zhao (DEVDIV) <dazhao@microsoft.com>
2020-02-19 11:55:06 -0800

    add debug info

--- SearchIndex/IndexServer/Controllers/HealthCheckController.cs
+++ SearchIndex/IndexServer/Controllers/HealthCheckController.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace IndexServer.Controllers
 {
     [ApiController]
     public class HealthCheckController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public HealthCheckController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet("health/live")]
-        public ActionResult GetLiveness() => Ok();
+        public ActionResult Liveness() => Ok(new { SearchServiceName = _configuration["SearchServiceName"], SearchIndexName = _configuration["SearchIndexName"] });
 
         [HttpGet("health/ready")]
-        public ActionResult GetReadiness() => Ok();
+        public ActionResult Readiness() => Ok(new { SearchServiceName = _configuration["SearchServiceName"], SearchIndexName = _configuration["SearchIndexName"] });
     }
 }
