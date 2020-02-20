4ced5ea582421ffb2df698ca3d2983e7cd6798c0
David Zhao (DEVDIV) <dazhao@microsoft.com>
2020-02-20 14:32:41 -0800

    save

---
+++ SearchIndex/LanguageModelBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace LanguageModelBuilder
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await Task.Yield();
+        }
+    }
+}
