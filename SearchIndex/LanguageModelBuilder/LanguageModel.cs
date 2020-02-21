9518391a3e84f260d8a79b74b0a513af4c1ac060
David Zhao (DEVDIV) <dazhao@microsoft.com>
2020-02-20 20:15:19 -0800

    save

---
+++ SearchIndex/LanguageModelBuilder/LanguageModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LanguageModelBuilder
+{
+    public class LanguageModel
+    {
+        private readonly Dictionary<string, Dictionary<(string, string), HashSet<Guid>>> _model = new Dictionary<string, Dictionary<(string, string), HashSet<Guid>>>();
+
+        public void Add(string term, string entityName, string attributeName, Guid recordId)
+        {
+            if (!_model.TryGetValue(term, out var bindings))
+            {
+                bindings = new Dictionary<(string, string), HashSet<Guid>>();
+                _model.Add(term, bindings);
+            }
+
+            if (!bindings.TryGetValue((entityName, attributeName), out var recordIds))
+            {
+                recordIds = new HashSet<Guid>();
+                bindings.Add((entityName, attributeName), recordIds);
+            }
+
+            recordIds.Add(recordId);
+        }
+
+        public IReadOnlyCollection<(string, string)> GetTermBindings(string term)
+        {
+            if (_model.TryGetValue(term, out var bindings))
+            {
+                return bindings.Keys;
+            }
+
+            return Array.Empty<(string, string)>();
+        }
+
+        public IReadOnlyCollection<Guid> GetRecordIds(string term, string entityName, string attributeName)
+        {
+            if (_model.TryGetValue(term, out var bindings))
+            {
+                if (bindings.TryGetValue((entityName, attributeName), out var recordIds))
+                {
+                    return recordIds;
+                }
+            }
+
+            return Array.Empty<Guid>();
+        }
+    }
+}
--- SearchIndex/LanguageModelBuilder/Program.cs
+++ SearchIndex/LanguageModelBuilder/Program.cs
@@ -1,7 +1,14 @@
-﻿using Microsoft.BizQA.NLU.QueryRewrite;
+﻿using CsvHelper;
+using IndexModels;
+using Microsoft.BizQA.NLU.QueryRewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LanguageModelBuilder
@@ -22,9 +29,80 @@ namespace LanguageModelBuilder
 
             var queryRewriter = QueryRewriteActivator.GetQueryRewriter(serviceProvider.GetRequiredService<IOptions<QueryRewriteOptions>>().Value);
 
-            var result = queryRewriter.Rewrite("David's hello world", QueryRewriteOperations.AllRegularOperations);
+            var model = await BuildLanguageModelAsync(queryRewriter, configuration["DataRootFolder"]);
 
-            await Task.Yield();
+            string query = "contact of ford";
+
+            foreach (var token in queryRewriter.Rewrite(query).LastStepRewriteTokens.Where(p => !p.IsStopword))
+            {
+                var bindings = model.GetTermBindings(token.Text);
+            }
+        }
+
+        private static async Task<LanguageModel> BuildLanguageModelAsync(IQueryRewriter queryRewriter, string dataRootFolder)
+        {
+            var model = new LanguageModel();
+
+            var entityMetadataArray = new[]
+            {
+                new { EntityName = Document.AccountEntityName, EntityIdName = Document.AccountEntityIdName },
+                new { EntityName = Document.BusinessUnitEntityName, EntityIdName = Document.BusinessUnitEntityIdName },
+                new { EntityName = Document.ContactEntityName, EntityIdName = Document.ContactEntityIdName },
+                new { EntityName = Document.LeadEntityName, EntityIdName = Document.LeadEntityIdName },
+                new { EntityName = Document.OpportunityEntityName, EntityIdName = Document.OpportunityEntityIdName },
+                new { EntityName = Document.OrganizationEntityName, EntityIdName = Document.OrganizationEntityIdName },
+                new { EntityName = Document.TerritoryEntityName, EntityIdName = Document.TerritoryEntityIdName },
+                new { EntityName = Document.UserEntityName, EntityIdName = Document.UserEntityIdName },
+            };
+
+            foreach (var entityMetadata in entityMetadataArray)
+            {
+                await BuildLanguageModelCoreAsync(model, queryRewriter, Path.Combine(dataRootFolder, entityMetadata.EntityName + ".csv"), entityMetadata.EntityName, entityMetadata.EntityIdName);
+            }
+
+            return model;
+        }
+
+        private static async Task BuildLanguageModelCoreAsync(LanguageModel model, IQueryRewriter queryRewriter, string csvFile, string entityName, string entityIdAttributeName)
+        {
+            var propertiesToIndex = Document.GetPropertiesToIndex(entityName);
+
+            int count = 0;
+
+            using (var streamReader = new StreamReader(csvFile))
+            {
+                using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    await foreach (IDictionary<string, object> entity in csvReader.GetRecordsAsync<dynamic>())
+                    {
+                        count++;
+
+                        if (!Guid.TryParse(entity[entityIdAttributeName]?.ToString(), out var entityId))
+                        {
+                            continue;
+                        }
+
+                        foreach (var propertyToIndex in propertiesToIndex)
+                        {
+                            string value = entity[propertyToIndex.CdsAttributeName]?.ToString();
+
+                            if (string.IsNullOrEmpty(value))
+                            {
+                                continue;
+                            }
+
+                            var result = queryRewriter.Rewrite(value);
+
+                            foreach (var token in result.LastStepRewriteTokens)
+                            {
+                                model.Add(token.Text, entityName, propertyToIndex.CdsAttributeName, entityId);
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"{count} records have been processed in {csvFile}.");
         }
     }
 }
