fb84e7eba39484bc09acfbde9c60f2374363b326
David Zhao (DEVDIV) <dazhao@microsoft.com>
2020-02-20 16:31:56 -0800

    save

--- SearchIndex/LanguageModelBuilder/Program.cs
+++ SearchIndex/LanguageModelBuilder/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using Microsoft.BizQA.NLU.QueryRewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
 
 namespace LanguageModelBuilder
 {
@@ -6,6 +10,20 @@ namespace LanguageModelBuilder
     {
         public static async Task Main(string[] args)
         {
+            var configurationBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+
+            var configuration = configurationBuilder.Build();
+
+            var services = new ServiceCollection();
+
+            services.Configure<QueryRewriteOptions>(configuration.GetSection(nameof(QueryRewriteOptions)));
+
+            var serviceProvider = services.BuildServiceProvider(true);
+
+            var tokenizer = QueryRewriteActivator.GetTokenizer(serviceProvider.GetRequiredService<IOptions<QueryRewriteOptions>>().Value);
+
+            var result = tokenizer.Tokenize("hello world");
+
             await Task.Yield();
         }
     }
